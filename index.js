7acd2d6f0438b669218ea1ce1f7b6f54dec72569
TheTrueTrident <thetruetrident@gmail.com>
2024-05-06 05:29:26 -0400

    scrapped lightmode, added Stella & Waelpro, extended list, began working on final touches.

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-<><script>
-    function play() { }
-    var audio = document.getElementById("audio");
-    audio.play();
+const themeToggleBtn = document.querySelector('.imgimg');
+
+
+const theme = localStorage.getItem('theme');
+
+var realtheme = document.querySelector(':root');
+
+console.log(theme);
+/*
+if (theme) {
+    realtheme.style.setProperty('--clr-dark', 'white')
+    realtheme.style.setProperty('--clr-light', 'black')
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode')
+        realtheme.style.setProperty('--clr-dark', 'white')
+        realtheme.style.setProperty('--clr-light', 'black')
+
+        console.log("light mode on")
+    } else {
+        localStorage.removeItem('theme')
+        console.log("dark mode off")
+        realtheme.style.setProperty('--clr-dark', 'black')
+        realtheme.style.setProperty('--clr-light', 'white')
     }
-</script><input type="button" value="PLAY" onclick="play()">
-        <audio id="audio" src="no3"></audio>
-    </script></>
+    
+})
+*\
